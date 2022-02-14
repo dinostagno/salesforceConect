8de53c290ab1224ce29ddf22618abf7791a82cfa
dinostagno <dpaolostagno@gmail.com>
2022-02-14 15:09:37 -0300

    values

--- controller.js
+++ controller.js
@@ -10,7 +10,7 @@ function accessDataSf() {
     http.open('POST', url, true);
 
     //Send the proper header information along with the request
-    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded', 'Access-Control-Allow-Origin', 'https://dinostagno.github.io');
 
     http.onreadystatechange = function() { //Call a function when the state changes.
         if (http.readyState == 4 && http.status == 200) {
